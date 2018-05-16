f39bdc8b014464faa420d41f366d76ebc3bf0bd6
Jonathan Lee <123abcjonathanlee@gmail.com>
2018-05-16 12:02:15 -0700

    redo dart experiments in test2

--- test2.sh
+++ test2.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config linear
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config linear
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config poly2
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config poly3
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config poly5
@@ -6,13 +6,13 @@ CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config poly7
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --config poly8
 
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config linear
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly2
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly3
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly5
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly6
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly7
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly8
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config linear
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly2
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly3
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly5
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly6
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly7
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly8
 
 CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --partition .10 --config linear
 CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname Walker2d-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --partition .10 --config poly2
@@ -26,7 +26,7 @@ CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname Walker2d-v1 
 
 
 
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config linear
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config linear
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config poly2
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config poly3
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config poly5
@@ -34,13 +34,13 @@ CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config poly7
 # CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --config poly8
 
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config linear
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly2
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly3
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly5
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly6
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly7
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly8
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config linear
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly2
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly3
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly5
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly6
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly7
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly8
 
 CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --partition .10 --config linear
 CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname Hopper-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --partition .10 --config poly2
@@ -54,21 +54,21 @@ CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname Hopper-v1 --
 
 
 
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config linear
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly2
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly3
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly5
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly6
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly7
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly8
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config linear
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly2
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly3
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly5
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly6
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly7
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_bc.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --config poly8
 
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config linear
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly2
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly3
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly5
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly6
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly7
-CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly8
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config linear
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly2
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly3
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly5
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly6
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly7
+# CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dagger.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --beta .5 --config poly8
 
 CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --partition .10 --config linear
 CUDA_VISIBLE_DEVICES=$NUM python experiments/test_dart.py --envname HalfCheetah-v1 --t 500 --max_data 400 --num_evals 8 --update_period 50 --partition .10 --config poly2
